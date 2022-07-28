2b908b12bf8656d607c43b4216c52495b4933911
Sadman Walid <40543547+SadmanWalid@users.noreply.github.com>
2022-07-28 16:39:19 +0600

    Configued Connection String and Scaffolded DbContext from DB

---
+++ SOMCH/Models/ScaffoledModels/RegDoctorRoomLog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SOMCH.Models.ScaffoledModels
+{
+    public partial class RegDoctorRoomLog
+    {
+        public string Id { get; set; } = null!;
+        public string? CreatedBy { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public bool Enabled { get; set; }
+        public string? UpdateBy { get; set; }
+        public DateTime? UpdateAt { get; set; }
+        public string? Doctorroomstatusenumkey { get; set; }
+    }
+}
